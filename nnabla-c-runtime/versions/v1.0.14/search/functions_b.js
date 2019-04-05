ea089bb9d115a2c42ed8f4d49814cd253579a21a
Yasunari Hashimoto <Yasunari.Hashimoto@sony.com>
2019-04-05 17:03:31 +0900

    Auto update at 2019年  4月  5日 金曜日 16:37:59 JST

---
+++ nnabla-c-runtime/versions/v1.0.14/search/functions_b.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['var_5fcalc_5foffset',['var_calc_offset',['../de/dc0/convolution__float_8c.html#a8c9ac2bf20b9d73c5382ceca69b86a6d',1,'var_calc_offset(var_t *var, int *pos, int size):&#160;convolution_float.c'],['../d1/de6/convolution__generic_8c.html#a8c9ac2bf20b9d73c5382ceca69b86a6d',1,'var_calc_offset(var_t *var, int *pos, int size):&#160;convolution_generic.c']]],
+  ['var_5ffree',['var_free',['../dd/ddc/convolution__common_8c.html#a9c9676ddb21752a02364c107c859d671',1,'convolution_common.c']]],
+  ['var_5fget',['var_get',['../d1/de6/convolution__generic_8c.html#ad3cd779cde4bd16435f177e6dcd50f76',1,'convolution_generic.c']]],
+  ['var_5fset',['var_set',['../d1/de6/convolution__generic_8c.html#a6fedd3198a46eb245014947d8e4123a7',1,'convolution_generic.c']]],
+  ['var_5fsetpos',['var_setpos',['../de/dc0/convolution__float_8c.html#abe28081b9dda1e2506d7473e2bf7c7df',1,'var_setpos(var_t *var, int *pos, int size):&#160;convolution_float.c'],['../d1/de6/convolution__generic_8c.html#abe28081b9dda1e2506d7473e2bf7c7df',1,'var_setpos(var_t *var, int *pos, int size):&#160;convolution_generic.c']]]
+];
---
+++ nnabla/versions/v1.0.14/doxygen/search/classes_d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['onehot',['OneHot',['../classnbla_1_1OneHot.html',1,'nbla']]],
+  ['optimizer',['Optimizer',['../classnbla_1_1utils_1_1nnp_1_1Optimizer.html',1,'nbla::utils::nnp']]],
+  ['outputvariable',['OutputVariable',['../structnbla_1_1utils_1_1nnp_1_1Executor_1_1OutputVariable.html',1,'nbla::utils::nnp::Executor']]]
+];
