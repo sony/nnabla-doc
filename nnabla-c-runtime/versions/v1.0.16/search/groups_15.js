11d06d6508937166dc90a65ae3eea65c1f481bdf
Yasunari Hashimoto <Yasunari.Hashimoto@sony.com>
2019-04-19 20:25:28 +0900

    Auto update at Fri Apr 19 19:20:51 JST 2019

---
+++ nnabla-c-runtime/versions/v1.0.16/search/groups_15.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['where',['Where',['../d1/d00/group__Where.html',1,'']]]
+];
---
+++ nnabla/versions/v1.0.16/doxygen/search/classes_11.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['tan',['Tan',['../classTan.html',1,'']]],
+  ['tanh',['Tanh',['../classTanh.html',1,'']]],
+  ['tile',['Tile',['../classnbla_1_1Tile.html',1,'nbla']]],
+  ['topkdata',['TopKData',['../classnbla_1_1TopKData.html',1,'nbla']]],
+  ['topkgrad',['TopKGrad',['../classnbla_1_1TopKGrad.html',1,'nbla']]],
+  ['topnerror',['TopNError',['../classnbla_1_1TopNError.html',1,'nbla']]],
+  ['trainingconfig',['TrainingConfig',['../classnbla_1_1utils_1_1nnp_1_1TrainingConfig.html',1,'nbla::utils::nnp']]],
+  ['transformbinary',['TransformBinary',['../classnbla_1_1TransformBinary.html',1,'nbla']]],
+  ['transformunary',['TransformUnary',['../classnbla_1_1TransformUnary.html',1,'nbla']]],
+  ['transpose',['Transpose',['../classnbla_1_1Transpose.html',1,'nbla']]]
+];
