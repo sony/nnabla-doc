c510c848421e92a523e0ed92cf4f8d30313a117d
github-merge <github-merge@examples.com>
2019-10-23 00:55:25 +0900

    Auto update at Wed Oct 23 00:44:03 JST 2019

---
+++ nnabla-c-runtime/versions/v1.3.0/search/enumvalues_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['shift_5fborder_5fmode_5fnearest',['SHIFT_BORDER_MODE_NEAREST',['../d3/d46/group__Shift.html#gga94b4d8cfcbb0e7fe87dc7d98ea6d7d6fa1aa4655f91c82eb74cf179f39a61ba89',1,'functions.h']]],
+  ['shift_5fborder_5fmode_5freflect',['SHIFT_BORDER_MODE_REFLECT',['../d3/d46/group__Shift.html#gga94b4d8cfcbb0e7fe87dc7d98ea6d7d6fa1f7ef1d72bb89b0f910f6ef72b092ce6',1,'functions.h']]],
+  ['sync_5fbatch_5fnormalization_5fgroup_5fworld',['SYNC_BATCH_NORMALIZATION_GROUP_WORLD',['../d9/d02/group__SyncBatchNormalization.html#gga8df324ba654e9205ee8520803e3e9967ac0642c2be1730c7fa0569f997174a43a',1,'functions.h']]]
+];
---
+++ nnabla/versions/v1.3.0/doxygen/search/all_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['klmultinomial',['KLMultinomial',['../classnbla_1_1KLMultinomial.html',1,'nbla']]]
+];
