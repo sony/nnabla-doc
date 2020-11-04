d76ee04644bf8909fdbfc971d3470ebf3d9babfe
github-merge <github-merge@examples.com>
2020-11-04 14:35:17 +0900

    Auto update at Wed Nov  4 14:21:25 JST 2020

---
+++ nnabla-c-runtime/versions/v1.13.0/search/classes_8.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['ifft_5flocal_5fcontext_5ft',['ifft_local_context_t',['../dc/d33/structifft__local__context__t.html',1,'']]],
+  ['image_5faugmentation_5flocal_5fcontext_5ft',['image_augmentation_local_context_t',['../d7/d3d/structimage__augmentation__local__context__t.html',1,'']]],
+  ['inq_5faffine_5flocal_5fcontext_5ft',['inq_affine_local_context_t',['../d5/da9/structinq__affine__local__context__t.html',1,'']]],
+  ['inq_5fconvolution_5flocal_5fcontext_5ft',['inq_convolution_local_context_t',['../d6/d2a/structinq__convolution__local__context__t.html',1,'']]],
+  ['interpolate_5flocal_5fcontext_5ft',['interpolate_local_context_t',['../d8/d7f/structinterpolate__local__context__t.html',1,'']]]
+];
---
+++ nnabla-c-runtime/versions/v1.13.0/search/files_5.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['file_5fformat_2emd',['FILE_FORMAT.md',['../d2/d16/FILE__FORMAT_8md.html',1,'']]],
+  ['fixedpoint_2ec',['fixedpoint.c',['../dd/d69/fixedpoint_8c.html',1,'']]],
+  ['fixedpoint_2eh',['fixedpoint.h',['../d8/dfd/fixedpoint_8h.html',1,'']]],
+  ['flip_2ec',['flip.c',['../d4/d75/flip_8c.html',1,'']]],
+  ['function_5fcontext_2ec',['function_context.c',['../db/dd5/function__context_8c.html',1,'']]],
+  ['functions_2eh',['functions.h',['../d8/d5c/functions_8h.html',1,'']]]
+];
