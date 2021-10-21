66063fb6a935dc9e17f295a310166aefd0911b04
github-merge <github-merge@examples.com>
2021-10-21 12:47:16 +0900

    Auto update at Thu Oct 21 12:34:20 JST 2021

---
+++ nnabla-c-runtime/versions/v1.22.0/search/files_c.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['selu_2ec',['selu.c',['../d8/d20/selu_8c.html',1,'']]],
+  ['shape_2ec',['shape.c',['../d8/d8c/shape_8c.html',1,'']]],
+  ['shape_2eh',['shape.h',['../da/d18/shape_8h.html',1,'']]],
+  ['shift_2ec',['shift.c',['../d5/dd1/shift_8c.html',1,'']]],
+  ['sigmoid_2ec',['sigmoid.c',['../dc/d57/sigmoid_8c.html',1,'']]],
+  ['sign_2ec',['sign.c',['../d6/d23/sign_8c.html',1,'']]],
+  ['slice_2ec',['slice.c',['../d0/d56/slice_8c.html',1,'']]],
+  ['softmax_2ec',['softmax.c',['../d8/d30/softmax_8c.html',1,'']]],
+  ['split_2ec',['split.c',['../d7/de7/split_8c.html',1,'']]],
+  ['stack_2ec',['stack.c',['../d3/dbb/stack_8c.html',1,'']]],
+  ['sub2_2ec',['sub2.c',['../d4/dce/sub2_8c.html',1,'']]],
+  ['sum_2ec',['sum.c',['../d4/d64/sum_8c.html',1,'']]],
+  ['sum_5fpooling_2ec',['sum_pooling.c',['../d0/de3/sum__pooling_8c.html',1,'']]],
+  ['support_5fstatus_2emd',['SUPPORT_STATUS.md',['../d8/db5/SUPPORT__STATUS_8md.html',1,'']]],
+  ['swish_2ec',['swish.c',['../d9/dad/swish_8c.html',1,'']]]
+];
---
+++ nnabla-c-runtime/versions/v1.22.0/search/groups_a.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['layernormalization',['LayerNormalization',['../de/de6/group__LayerNormalization.html',1,'']]],
+  ['leakyrelu',['LeakyReLU',['../d7/da7/group__LeakyReLU.html',1,'']]],
+  ['less',['Less',['../d3/dbe/group__Less.html',1,'']]],
+  ['lessequal',['LessEqual',['../db/da1/group__LessEqual.html',1,'']]],
+  ['lessequalscalar',['LessEqualScalar',['../d6/dad/group__LessEqualScalar.html',1,'']]],
+  ['lessscalar',['LessScalar',['../d5/d68/group__LessScalar.html',1,'']]],
+  ['log',['Log',['../de/d7a/group__Log.html',1,'']]],
+  ['logical',['Logical',['../d5/db6/group__Logical.html',1,'']]],
+  ['logicaland',['LogicalAnd',['../de/d44/group__LogicalAnd.html',1,'']]],
+  ['logicalandscalar',['LogicalAndScalar',['../d0/dd5/group__LogicalAndScalar.html',1,'']]],
+  ['logicalnot',['LogicalNot',['../dd/d26/group__LogicalNot.html',1,'']]],
+  ['logicalor',['LogicalOr',['../d4/dae/group__LogicalOr.html',1,'']]],
+  ['logicalorscalar',['LogicalOrScalar',['../d8/ddc/group__LogicalOrScalar.html',1,'']]],
+  ['logicalxor',['LogicalXor',['../d7/d23/group__LogicalXor.html',1,'']]],
+  ['logicalxorscalar',['LogicalXorScalar',['../d6/d88/group__LogicalXorScalar.html',1,'']]],
+  ['logsigmoid',['LogSigmoid',['../d2/de6/group__LogSigmoid.html',1,'']]],
+  ['logsoftmax',['LogSoftmax',['../d1/d63/group__LogSoftmax.html',1,'']]],
+  ['loss_20functions',['Loss Functions',['../d6/d81/group__LossFunctions.html',1,'']]],
+  ['lstm',['LSTM',['../d8/d86/group__LSTM.html',1,'']]]
+];
