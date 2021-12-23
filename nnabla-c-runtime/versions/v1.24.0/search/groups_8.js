78345a12197735ea3a8baf7da76842462abc73e4
github-merge <github-merge@examples.com>
2021-12-23 16:45:59 +0900

    Auto update at Thu Dec 23 16:07:26 JST 2021

---
+++ nnabla-c-runtime/versions/v1.24.0/search/groups_8.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['identity',['Identity',['../df/d7a/group__Identity.html',1,'']]],
+  ['ifft',['IFFT',['../de/da7/group__IFFT.html',1,'']]],
+  ['imageaugmentation',['ImageAugmentation',['../d9/d49/group__ImageAugmentation.html',1,'']]],
+  ['individual_20functions',['Individual Functions',['../d8/da3/group__IndividualFunctions.html',1,'']]],
+  ['inqaffine',['INQAffine',['../d4/db7/group__INQAffine.html',1,'']]],
+  ['inqconvolution',['INQConvolution',['../d4/d89/group__INQConvolution.html',1,'']]],
+  ['instancenormalization',['InstanceNormalization',['../db/d2f/group__InstanceNormalization.html',1,'']]],
+  ['interpolate',['Interpolate',['../d2/d80/group__Interpolate.html',1,'']]],
+  ['isinf',['IsInf',['../d3/df8/group__IsInf.html',1,'']]],
+  ['isnan',['IsNaN',['../df/da7/group__IsNaN.html',1,'']]],
+  ['istft',['ISTFT',['../d4/d5c/group__ISTFT.html',1,'']]],
+  ['internal_20network_20representation',['Internal network representation',['../d8/d35/group__Network.html',1,'']]]
+];
---
+++ nnabla/versions/v1.24.0/_sources/python/api/grad.rst.txt
@@ -0,0 +1,10 @@
+Grad
+====
+
+.. automodule:: nnabla.grad
+
+.. autofunction:: nnabla.grad.grad
+
+.. autofunction:: nnabla.backward_functions.register
+
+.. autofunction:: nnabla.backward_functions.show_registry
