3f3ee8c15ab6cc5cb81369be111f07c6d0e5661d
github-merge <github-merge@examples.com>
2022-04-10 12:08:35 +0900

    Auto update at Sun Apr 10 11:53:45 JST 2022

---
+++ nnabla-c-runtime/versions/v1.27.0/search/classes_12.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['tensor_5fnormalization_5flocal_5fcontext_5ft',['tensor_normalization_local_context_t',['../d1/de5/structtensor__normalization__local__context__t.html',1,'']]],
+  ['tile_5flocal_5fcontext_5ft',['tile_local_context_t',['../da/d0f/structtile__local__context__t.html',1,'']]],
+  ['top_5fk_5fdata_5flocal_5fcontext_5ft',['top_k_data_local_context_t',['../d2/dc0/structtop__k__data__local__context__t.html',1,'']]],
+  ['top_5fk_5fgrad_5flocal_5fcontext_5ft',['top_k_grad_local_context_t',['../de/d8a/structtop__k__grad__local__context__t.html',1,'']]],
+  ['top_5fn_5ferror_5flocal_5fcontext_5ft',['top_n_error_local_context_t',['../d0/d83/structtop__n__error__local__context__t.html',1,'']]],
+  ['transpose_5flocal_5fcontext_5ft',['transpose_local_context_t',['../d4/daf/structtranspose__local__context__t.html',1,'']]]
+];
---
+++ nnabla-c-runtime/versions/v1.27.0/search/pages_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['support_5fstatus',['SUPPORT_STATUS',['../db/da4/md_doc_SUPPORT_STATUS.html',1,'']]]
+];
