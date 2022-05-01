1686920a5507a39df36c9cfeba47322627b227d4
github-merge <github-merge@examples.com>
2022-05-01 21:19:03 +0900

    Auto update at Sun May  1 21:03:27 JST 2022

---
+++ nnabla-c-runtime/versions/v1.28.0/search/all_14.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['usage',['USAGE',['../da/d6f/md_doc_USAGE.html',1,'']]],
+  ['unimplemented_2ec',['unimplemented.c',['../d8/d45/unimplemented_8c.html',1,'']]],
+  ['unlink',['Unlink',['../d5/d67/group__Unlink.html',1,'']]],
+  ['unpooling',['Unpooling',['../de/d75/group__Unpooling.html',1,'']]],
+  ['unpooling_2ec',['unpooling.c',['../de/d0c/unpooling_8c.html',1,'']]],
+  ['unpooling_5flocal_5fcontext_5ft',['unpooling_local_context_t',['../d9/d0a/structunpooling__local__context__t.html',1,'']]],
+  ['update_5frunning_5fmean',['update_running_mean',['../d1/d5a/structmean__subtraction__local__context__t.html#af789c6706ddd8d2e9ca8a7370a10a8ed',1,'mean_subtraction_local_context_t::update_running_mean()'],['../d4/d31/structnn__function__mean__subtraction__t.html#a110fcdeba002fa239122e188156aa828',1,'nn_function_mean_subtraction_t::update_running_mean()']]],
+  ['upper',['upper',['../dc/d70/structbatch__cholesky__local__context__t.html#aa910bf26a26cb6b921a9d38958ee78a4',1,'batch_cholesky_local_context_t::upper()'],['../df/d24/structnn__function__batch__cholesky__t.html#a633075aa8902a252e5184a4f4da38ba6',1,'nn_function_batch_cholesky_t::upper()']]],
+  ['usage_2emd',['USAGE.md',['../dc/def/USAGE_8md.html',1,'']]]
+];
