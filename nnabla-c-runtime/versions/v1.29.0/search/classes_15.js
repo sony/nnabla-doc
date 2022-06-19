26a3a29d6d38ca515c9aec9d4404a5f82c94b475
github-merge <github-merge@examples.com>
2022-06-19 21:56:37 +0900

    Auto update at Sun Jun 19 21:22:04 JST 2022

---
+++ nnabla-c-runtime/versions/v1.29.0/search/classes_15.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['warp_5fby_5fgrid_5flocal_5fcontext_5ft',['warp_by_grid_local_context_t',['../d5/d16/structwarp__by__grid__local__context__t.html',1,'']]],
+  ['weight_5fnormalization_5flocal_5fcontext_5ft',['weight_normalization_local_context_t',['../d9/d4b/structweight__normalization__local__context__t.html',1,'']]],
+  ['weight_5fstandardization_5flocal_5fcontext_5ft',['weight_standardization_local_context_t',['../d1/d50/structweight__standardization__local__context__t.html',1,'']]]
+];
