6fdbfe78415ed1b8301080b322d1b3a667a35a06
github-merge <github-merge@examples.com>
2022-10-07 16:04:34 +0900

    Auto update at Fri Oct  7 15:38:18 JST 2022

---
+++ nnabla-c-runtime/versions/v1.31.0/search/groups_f.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['quantization_20neural_20network_20layers',['Quantization Neural Network Layers',['../df/d15/group__QuantizationNeuralNetworkLayers.html',1,'']]],
+  ['quantizelinear',['QuantizeLinear',['../d9/d0c/group__QuantizeLinear.html',1,'']]]
+];
