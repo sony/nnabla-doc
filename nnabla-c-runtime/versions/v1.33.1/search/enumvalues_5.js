24377523087e16af52aed86be57300d939dad662
github-merge <github-merge@examples.com>
2023-02-09 13:47:48 +0900

    Auto update at Thu Feb  9 13:20:06 JST 2023

---
+++ nnabla-c-runtime/versions/v1.33.1/search/enumvalues_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['quantize_5flinear_5fround_5fmode_5fhalf_5faway_5ffrom_5fzero',['QUANTIZE_LINEAR_ROUND_MODE_HALF_AWAY_FROM_ZERO',['../d9/d0c/group__QuantizeLinear.html#gga74164e782465d68a5decc6de92b6fd24a8ca26ba101691beb24508d30e1207d37',1,'functions.h']]],
+  ['quantize_5flinear_5fround_5fmode_5fhalf_5fto_5feven',['QUANTIZE_LINEAR_ROUND_MODE_HALF_TO_EVEN',['../d9/d0c/group__QuantizeLinear.html#gga74164e782465d68a5decc6de92b6fd24a27cfbd7d6ac10ef7e71c89232cec82cd',1,'functions.h']]]
+];
