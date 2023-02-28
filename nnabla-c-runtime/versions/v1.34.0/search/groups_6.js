d0a585d1d8aaae63c473e97ca1f68c0d146856ce
github-merge <github-merge@examples.com>
2023-02-28 11:28:28 +0900

    Auto update at Tue Feb 28 11:14:19 JST 2023

---
+++ nnabla-c-runtime/versions/v1.34.0/search/groups_6.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['gather',['Gather',['../dc/d8e/group__Gather.html',1,'']]],
+  ['gathernd',['GatherNd',['../dc/da5/group__GatherNd.html',1,'']]],
+  ['gelu',['GELU',['../dc/ded/group__GELU.html',1,'']]],
+  ['geometric_20neural_20network_20layers',['Geometric Neural Network Layers',['../d6/dfa/group__GeometricNeuralNetworkLayers.html',1,'']]],
+  ['globalaveragepooling',['GlobalAveragePooling',['../d3/dca/group__GlobalAveragePooling.html',1,'']]],
+  ['greater',['Greater',['../d0/d1b/group__Greater.html',1,'']]],
+  ['greaterequal',['GreaterEqual',['../df/db5/group__GreaterEqual.html',1,'']]],
+  ['greaterequalscalar',['GreaterEqualScalar',['../dd/da1/group__GreaterEqualScalar.html',1,'']]],
+  ['greaterscalar',['GreaterScalar',['../d8/d91/group__GreaterScalar.html',1,'']]],
+  ['groupnormalization',['GroupNormalization',['../d4/de1/group__GroupNormalization.html',1,'']]],
+  ['gru',['GRU',['../dc/d00/group__GRU.html',1,'']]]
+];
---
+++ nnabla/versions/v1.34.0/_sources/python/api/communicator.rst.txt
@@ -0,0 +1,20 @@
+Communicator
+============
+
+Communicator transfers parameters over the compute graphs.  
+
+
+.. automodule:: nnabla.communicators
+
+
+Communicator interface
+----------------------
+
+.. autoclass:: Communicator
+    :members:
+
+List of communicators
+---------------------
+
+.. autofunction:: DataParalellCommunicator
+.. autofunction:: MultiProcessDataParalellCommunicator
