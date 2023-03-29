85f410b190af9fc3260f958c06485380cd7a2564
github-merge <github-merge@examples.com>
2023-03-29 20:33:44 +0900

    Auto update at Wed Mar 29 19:26:04 JST 2023

---
+++ nnabla-c-runtime/versions/v1.35.0/search/variables_16.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['x',['x',['../db/d19/structpooling__calc__context__t.html#a9527b9b51671d5975dc8d65a2f89c80f',1,'pooling_calc_context_t']]],
+  ['x_5fmap_5fsize',['x_map_size',['../d6/d45/structpooling__private__t.html#a6c74e9f9c0aa67783b6d41d8e4514f24',1,'pooling_private_t']]],
+  ['x_5fmin_5fmax',['x_min_max',['../df/d8e/structmin__max__quantize__local__context__t.html#ab4f3d077bf5d4c125e83c70dbd5d38a5',1,'min_max_quantize_local_context_t::x_min_max()'],['../db/d07/structnn__function__min__max__quantize__t.html#a142387e9e865645ada7afb7308dc9724',1,'nn_function_min_max_quantize_t::x_min_max()']]]
+];
---
+++ nnabla/versions/v1.35.0/_sources/python/api/monitor.rst.txt
@@ -0,0 +1,31 @@
+Monitors
+========
+
+The Monitor API provides helpers for logging the progress of neural network training.
+
+.. automodule:: nnabla.monitor
+.. autoclass:: Monitor
+  :members:
+
+.. _monitors:
+
+List of Monitors
+----------------
+
+.. autoclass:: MonitorSeries
+  :members:
+.. autoclass:: MonitorTimeElapsed
+  :members:
+.. autoclass:: MonitorImage
+  :members:
+.. autoclass:: MonitorImageTile
+  :members:
+
+Utility functions
+-----------------
+
+.. autofunction:: tile_images
+
+.. autofunction:: plot_series
+
+.. autofunction:: plot_time_elapsed
