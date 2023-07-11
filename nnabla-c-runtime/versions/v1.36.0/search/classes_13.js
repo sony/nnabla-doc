ad965edb80c58d4e401c99ed8502a7f14c21d12b
github-merge <github-merge@examples.com>
2023-07-11 13:56:50 +0900

    Auto update at Tue Jul 11 12:31:08 JST 2023

---
+++ nnabla-c-runtime/versions/v1.36.0/search/classes_13.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['unpooling_5flocal_5fcontext_5ft',['unpooling_local_context_t',['../d9/d0a/structunpooling__local__context__t.html',1,'']]]
+];
---
+++ nnabla-c-runtime/versions/v1.36.0/search/classes_d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['one_5fhot_5flocal_5fcontext_5ft',['one_hot_local_context_t',['../d6/d60/structone__hot__local__context__t.html',1,'']]],
+  ['onnx_5fnon_5fmax_5fsuppression_5flocal_5fcontext_5ft',['onnx_non_max_suppression_local_context_t',['../da/d87/structonnx__non__max__suppression__local__context__t.html',1,'']]],
+  ['onnx_5fresize_5flocal_5fcontext_5ft',['onnx_resize_local_context_t',['../de/d57/structonnx__resize__local__context__t.html',1,'']]]
+];
